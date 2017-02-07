e8d4ff102c046dc8c65c7ad1de47d728305d406e
Alajuwon Thomas <spooda7@yahoo.com>
2017-02-07 18:05:56 -0500

    added a few models for tweets and TweetTag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206143250) do
+ActiveRecord::Schema.define(version: 20170207145619) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "phrase"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", force: :cascade do |t|
+    t.integer  "tweet_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
@@ -36,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170206143250) do
     t.string   "username"
     t.text     "bio"
     t.string   "location"
+    t.text     "following"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
